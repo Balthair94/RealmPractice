7f6f22e6dd17a2f83048fc42fa516bd61283ed1b
balthair94 <archerbaltazar@gmail.com>
2017-02-24 01:08:36 -0600

    View FriendDetail added

---
+++ app/src/main/java/baltamon/mx/realmpractice/FriendDetailActivity.java
@@ -0,0 +1,58 @@
+package baltamon.mx.realmpractice;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Button;
+
+/**
+ * Created by Baltazar Rodriguez on 24/02/2017.
+ */
+
+public class FriendDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_detail);
+
+        setUpToolbar();
+        startingObjects();
+    }
+
+    private void startingObjects() {
+        Button button = (Button) findViewById(R.id.btnActionButton);
+        button.setText("Update friend");
+    }
+
+    private void setUpToolbar() {
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null){
+            actionbar.setTitle("Friend Detail");
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            onBackPressed();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
