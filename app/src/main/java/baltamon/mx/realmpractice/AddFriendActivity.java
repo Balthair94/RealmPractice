a51b061b578a5fa3878a7f9c99e2711725b4e845
balthair94 <archerbaltazar@gmail.com>
2017-02-24 00:55:23 -0600

    Add Friend view added

---
+++ app/src/main/java/baltamon/mx/realmpractice/AddFriendActivity.java
@@ -0,0 +1,51 @@
+package baltamon.mx.realmpractice;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by Baltazar Rodriguez on 24/02/2017.
+ */
+
+public class AddFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend);
+
+        setUpToolbar();
+    }
+
+    private void setUpToolbar() {
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null){
+            actionbar.setTitle("Add friend");
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            onBackPressed();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
--- app/src/main/java/baltamon/mx/realmpractice/MainActivity.java
+++ app/src/main/java/baltamon/mx/realmpractice/MainActivity.java
@@ -1,7 +1,13 @@
 package baltamon.mx.realmpractice;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUPToolbar();
+        setUpFloatingButton();
+    }
+
+    private void setUPToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setTitle("My friends");
+        }
+    }
+
+    private void setUpFloatingButton() {
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddFriendActivity.class));
+            }
+        });
     }
 }
